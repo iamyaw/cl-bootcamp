dbdca2c8ea06509127503916d758d1686763f698
Sean Cavanaugh <sean@cavanaugh.pro>
2015-02-25 14:09:48 -0500

    sync some more puppet

---
+++ puppet-ospf-unnumbered/etc/puppet/modules/base/manifests/role/switch.pp
@@ -0,0 +1,13 @@
+class base::role::switch {
+  include stdlib
+
+  class { 'base::license':
+    stage => 'setup',
+  }
+
+  include base::interfaces,
+    base::motd,
+    base::ntpclient,
+    base::users,
+    base::sysctl
+}
